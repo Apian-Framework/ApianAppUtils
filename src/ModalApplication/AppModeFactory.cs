d368cfe627c571c6e7ead54c5e943edab1a983a9
jim berry <jim@bigfattail.com>
2022-05-10 11:27:18 -0400

    Added name-based methods to mode factory

--- src/ModalApplication/AppModeFactory.cs
+++ src/ModalApplication/AppModeFactory.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System;
 
 namespace ModalApplication
@@ -6,15 +7,24 @@ namespace ModalApplication
 	public interface IAppModeFactory
 	{
 		IAppMode Create(int modeId);
+		IAppMode Create(string modeName);
+
+		int ModeIdForName( string name);
+
+		string ModeName(int id);
 	}
 
 	public abstract class AppModeFactory : IAppModeFactory
 	{
 		protected Dictionary<int, Func<IAppMode>> AppModeCtors;
-        public IAppMode Create(int modeId)
-        {
-            return AppModeCtors[modeId]();
-        }
+		protected Dictionary<int, string> AppModeNames;
+
+        public IAppMode Create(int modeId) => AppModeCtors[modeId]();
+        public IAppMode Create(string modeName) => AppModeCtors[ModeIdForName(modeName)]();
+
+		public int ModeIdForName(string name) => AppModeNames.First(x => x.Value == name).Key;
+		public string ModeName(int id) => AppModeNames[id];
+
 	};
 }
 
